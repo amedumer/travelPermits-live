8fcd7d02acf2ae39c43a0d647af84b9ab6ac5fb0
amedumer <ahmetomer@sabanciuniv.edu>
2021-08-17 02:52:32 +0300

    Updates

---
+++ precache-manifest.2138931569be20b3350a3fe37122297f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7429c3b8d3dd957fde6",
+    "url": "/travelPermits-live/static/css/main.7a180f28.chunk.css"
+  },
+  {
+    "revision": "f7429c3b8d3dd957fde6",
+    "url": "/travelPermits-live/static/js/main.f7429c3b.chunk.js"
+  },
+  {
+    "revision": "733a12327db049886390",
+    "url": "/travelPermits-live/static/css/1.95c244f2.chunk.css"
+  },
+  {
+    "revision": "733a12327db049886390",
+    "url": "/travelPermits-live/static/js/1.733a1232.chunk.js"
+  },
+  {
+    "revision": "d0e2271f20b5c6569298",
+    "url": "/travelPermits-live/static/js/runtime~main.d0e2271f.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/travelPermits-live/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "968b2cf7b5313bc03135822a872481e1",
+    "url": "/travelPermits-live/index.html"
+  }
+];
